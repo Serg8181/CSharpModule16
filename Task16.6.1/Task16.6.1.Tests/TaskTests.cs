18eb924f2733c96c5c141e2904e4fb1dcbbe9dc4
Serg8181 <147308625+Serg8181@users.noreply.github.com>
2024-06-20 10:53:28 +0300

    Task16.6.1

---
+++ Task16.6.1/Task16.6.1.Tests/TaskTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task16._6._1.Tests
+{
+    [TestFixture]
+    public class TaskTests
+    {
+        [Test]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            var calc = new Calculator();
+            Assert.That(calc.Additional(200, 100) == 300);
+        }
+
+        [Test]
+        public void SubtractionMustReturnCorrectValue()
+        {
+            var calc = new Calculator();
+            Assert.That(calc.Subtraction(200, 100) == 100);
+        }
+
+        [Test]
+        public void MiltiplicationMustReturnCorrectValue()
+        {
+            var calc = new Calculator();
+            Assert.That(calc.Miltiplication(5, 5) == 25);
+        }
+
+        [Test]
+        public void DivisionMustReturnCorrectValue()
+        {
+            var calc = new Calculator();
+            Assert.That(calc.Division(5, 5) == 1);
+           
+        }
+
+        [Test]
+        public void DivisionMustThrowException()
+        {
+            var calc = new Calculator();
+            Assert.Throws<DivideByZeroException>(()=>calc.Division(30,0));
+
+        }
+
+
+    }
+}
---
+++ Task16.6.1/Task16.6.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Task16._6._1
+{
+
+    public class Programm
+    {
+
+        public static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
